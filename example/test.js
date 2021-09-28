8e29ddbb2b55fa67dfe82dba591ed220e4abcea5
Adelmo júnior <junior-adelmo@hotmail.com>
2021-09-28 09:33:01 -0300

    updating ./example/test.js

--- example/test.js
+++ example/test.js
@@ -1,6 +1,6 @@
-const high = require('./index.js')
+const high = require('../index.js')
 const app = high()
-const http = require('http')
+
 app.listen(3000, () => {
   console.log('Server is working')
 })
